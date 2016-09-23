0098246c8254f70c8cdfbf57fc7dad55cb6f9c34
Marshall Thompson <marshallthomp@gmail.com>
2016-09-23 14:42:52 -0400

    got percentage based hex values working

--- js/app.js
+++ js/app.js
@@ -35,13 +35,45 @@
 	}
 
 	function padZero(num){
-		var paddedNum = ("0" + num).slice(-2)
+		var paddedNum = ("0" + num).slice(-2);
+
 		return paddedNum;
 	}
 
-	function convertToHex(num){
-		var hexify = num.toString(16);
-		return hexify;
+	function convertTimeToHex(num){
+
+		var currentHour = logCurrentTime().hour;
+		var currentMinute = logCurrentTime().minute;
+		var currentSecond = logCurrentTime().second;
+
+		// set up ms values
+		var msDay = 3600000 * 24;
+		var msHour = 60000 * 60;
+		var msMinute = 60000;
+		var msSecond = 1000;
+
+		
+		// find time percentages
+		// find the percentage of day completed
+		var percentOfDayCompleteBase16 = 		(Math.floor(((currentHour * msHour)/ msDay) * 100))
+																						 .toString(16);
+    // find the percentage of hour completed
+		var percentOfHourCompleteBase16 = 	(Math.floor(((currentMinute * msMinute)/ msHour) * 100))
+																				     .toString(16);
+    // find the percentage of minute completed
+		var percentOfMinuteCompleteBase16 = (Math.floor(((currentSecond * msSecond) / msMinute) * 100))
+																						 .toString(16);
+		// make hex code based off of percentages
+		var hexCat = '#';
+				hexCat += padZero(percentOfDayCompleteBase16);
+				hexCat += padZero(percentOfHourCompleteBase16);
+				hexCat += padZero(percentOfMinuteCompleteBase16);
+
+				console.log(hexCat);
+		
+		return {
+						hexPercent: hexCat
+					 };
 	}
 
 	function displayTime(){
@@ -53,16 +85,19 @@
 
 	}
 
-	function displayColor() {
-		var timeLog = logCurrentTime();
+	function setColor() {
+		
+		var hrsText = hours.textContent;
+		var minText = minutes.textContent;
+		var secText = seconds.textContent;
 
-		var hex = "#";
-				hex += convertToHex(timeLog.hour);
-				hex += convertToHex(timeLog.minute);
-				hex += convertToHex(timeLog.second);
+		var hex = convertTimeToHex().hexPercent;
+		 		// hex += convertToHex(minText);
+		 		// hex += convertToHex(secText);
 
-		wrapper.style.backgroundColor = hex;
-		console.log("applying color: " + hex )
+		wrapper.setAttribute('style', 'background-color:' + hex);
+		console.log("applying color: " + hex );
+		// console.log("applying color: " + hex );
 
 	}
 
@@ -70,21 +105,29 @@
 		var currentTime = logCurrentTime();
 		var seconds = currentTime.second;
 
-
-
 		secondsBar.style.width = (seconds / 60) * 100 + '%';
 	}
 
 
+// group all function calls into one function
+	function colorClock(){
+		displayTime();
+		setColor();
+		growSecondsBar();
+	}
 
-	// cease the 1 second lag on load 
-	window.addEventListener('load', displayTime);
-	window.addEventListener('load', displayColor);
+
+
+	// initialize the clock on load 
+	// window.addEventListener('load', displayTime);
+	// window.addEventListener('load', setColor);
+	window.addEventListener('load', colorClock);
 	
 	// set timers on the window object
-	window.setInterval(displayTime, 1000);
-	window.setInterval(displayColor, 1000);
-	window.setInterval(growSecondsBar, 1000);
+	// window.setInterval(displayTime, 1000);
+	// window.setInterval(setColor, 1000);
+	// window.setInterval(growSecondsBar, 1000);
+	window.setInterval(colorClock, 1000);
 	
 
 
